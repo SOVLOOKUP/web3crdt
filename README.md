bd7f487e0e5d77e3b0980aa93a5215e8aa794e9e
sovlookup <gonorth@qq.com>
2024-05-12 21:47:37 +0800

    0.0.1

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ import { Loro } from "loro-crdt"
 const doc = new Loro()
 // Device are automatically connected to the IPFS network 
 // All docs with the same sync ID will be automatically synchronized
-sync(doc, "your-sync-id")
+await sync(doc, "your-sync-id")
 ```
--- test.ts
+++ test.ts
@@ -2,17 +2,20 @@ import { Loro, LoroList } from "loro-crdt"
 import { sync } from "./src"
 
 const a = new Loro()
-console.log(a.peerIdStr)
-sync(a, "testloro111")
+const stop = await sync(a, "testloro111")
 
 const map = a.getMap("map");
 const list = map.setContainer("list221", new LoroList());
 
 let i = 0
 
-setInterval(() => {
+setInterval(async () => {
     list.push(i);
     a.commit()
     i++
     console.log(a.toJSON())
+
+    if (i > 10) {
+        await stop()
+    }
 }, 3000)
